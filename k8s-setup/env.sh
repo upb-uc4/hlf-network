d9926754a7cb272b15b24537d6c1fd8fa3da177e
Paul Kramer <pkramer1503@gmail.com>
2020-06-06 17:18:21 +0200

    Work on registration of CA admin.

--- k8s-setup/env.sh
+++ k8s-setup/env.sh
@@ -1,2 +1,6 @@
-export TMP_FOLDER=tmp-certificates                                              
+export TMP_FOLDER=tmp
+export CA_CLIENT=/../bin-setup/fabric-ca-client/fabric-ca-client
+export FABRIC_CA_CLIENT_TLS_CERTFILES=tls-ca-cert.pem
+export FABRIC_CA_CLIENT_HOME=$TMP_FOLDER/ca-client-home
+
 
--- k8s-setup/startNetwork.sh
+++ k8s-setup/startNetwork.sh
@@ -15,20 +15,32 @@ source ./env.sh
 # Starting the Network
 echo Starting Network
 minikube start
+
 sep
 
 echo Creating TLS CA pod
 kubectl apply -f tls-ca-server.yaml
+sleep 5
+
 sep
 
 echo Pod Names
 TLS_CA_NAME=$(get_pods)
+echo $TLS_CA_NAME
+
 sep
 
 echo Copy TLS certificate to local folder 
 mkdir -p $TMP_FOLDER
-
-
-echo $POD
 kubectl cp default/$TLS_CA_NAME:etc/hyperledger/fabric-ca-server/ca-cert.pem $TMP_FOLDER/ca-cert.pem
+sleep 2
+
+sep
+
+echo Use CA-client to enroll admin
+cp $TMP_FOLDER/ca-cert.pem $FABRIC_CA_CLIENT_HOME/$FABRIC_CA_CLIENT_TLS_CERTFILES
+echo $FABRIC_CA_CLIENT_HOME/$FABRIC_CA_CLIENT_TLS_CERTFILES
+kubectl expose deployment ca-tls --port=7052  # TODO add a Service to open port
+./$CA_CLIENT enroll -d -u https://tls-ca-admin:tls-ca-adminpw@0.0.0.0:7052
+
 
