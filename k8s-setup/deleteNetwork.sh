9363a325b859d7f1382b6e1b57d93e80d2088947
Jonas Thiele <JonasThi97@gmail.com>
2020-06-09 13:38:38 +0200

    Fix tls ca service exposure

--- k8s-setup/deleteNetwork.sh
+++ k8s-setup/deleteNetwork.sh
@@ -3,7 +3,10 @@
 source ./env.sh
 
 # Delete CA Server
-kubectl delete -f tls-ca-server.yaml
+kubectl delete -f tls-ca.yaml
+
+# Delete CA Service
+kubectl delete -f tls-ca-service.yaml
 
 # Remove tmp dir
 rm -rf $TMP_FOLDER
--- k8s-setup/startNetwork.sh
+++ k8s-setup/startNetwork.sh
@@ -1,7 +1,7 @@
 # Function definitions
 
 get_pods() {
-    kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}'
+    kubectl get pods -l app=ca-tls-root --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}'
 }
 
 sep() {
@@ -18,8 +18,14 @@ minikube start
 
 sep
 
+#echo Creating TLS CA service
+#kubectl create -f tls-ca-service.yaml
+#sleep 2
+
+#sep
+
 echo Creating TLS CA pod
-kubectl apply -f tls-ca-server.yaml
+kubectl create -f tls-ca.yaml
 sleep 5
 
 sep
@@ -30,7 +36,7 @@ echo $TLS_CA_NAME
 
 sep
 
-echo Copy TLS certificate to local folder 
+echo Copy TLS certificate to local folder
 mkdir -p $TMP_FOLDER
 kubectl cp default/$TLS_CA_NAME:etc/hyperledger/fabric-ca-server/ca-cert.pem $TMP_FOLDER/ca-cert.pem
 sleep 2
@@ -40,7 +46,11 @@ sep
 echo Use CA-client to enroll admin
 cp $TMP_FOLDER/ca-cert.pem $FABRIC_CA_CLIENT_HOME/$FABRIC_CA_CLIENT_TLS_CERTFILES
 echo $FABRIC_CA_CLIENT_HOME/$FABRIC_CA_CLIENT_TLS_CERTFILES
-kubectl expose deployment ca-tls --port=7052  # TODO add a Service to open port
-./$CA_CLIENT enroll -d -u https://tls-ca-admin:tls-ca-adminpw@0.0.0.0:7052
+kubectl expose deployment ca-tls --type=NodePort --name=ca-tls --port 7052
+CA_SERVER_HOST=$(minikube service ca-tls --url | cut -c 8-)
+echo $CA_SERVER_HOST
+./$CA_CLIENT enroll -d -u https://tls-ca-admin:tls-ca-adminpw@$CA_SERVER_HOST
+
+
 
 
