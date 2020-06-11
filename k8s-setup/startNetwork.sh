8f8f90296a84c021d96ce4ff3c66313bb5f7871d
Jonas Thiele <JonasThi97@gmail.com>
2020-06-11 18:04:32 +0200

    Refactor startNetwork script

--- k8s-setup/startNetwork.sh
+++ k8s-setup/startNetwork.sh
@@ -18,37 +18,30 @@ minikube start
 
 sep
 
-#echo Creating TLS CA service
-#kubectl create -f tls-ca-service.yaml
-#sleep 2
-
-#sep
-
 echo Creating TLS CA pod
 kubectl create -f tls-ca.yaml
-sleep 5
+TLS_CA_NAME=$(get_pods)
+echo Pod $TLS_CA_NAME created
 
 sep
 
-echo Pod Names
-TLS_CA_NAME=$(get_pods)
-echo $TLS_CA_NAME
+echo Creating TLS CA service
+kubectl create -f tls-ca-service.yaml
+CA_SERVER_HOST=$(minikube service ca-tls --url | cut -c 8-)
+echo TLS CA service exposed on $CA_SERVER_HOST
 
 sep
 
 echo Copy TLS certificate to local folder
 mkdir -p $TMP_FOLDER
 kubectl cp default/$TLS_CA_NAME:etc/hyperledger/fabric-ca-server/ca-cert.pem $TMP_FOLDER/ca-cert.pem
-sleep 2
 
 sep
 
 echo Use CA-client to enroll admin
 cp $TMP_FOLDER/ca-cert.pem $FABRIC_CA_CLIENT_HOME/$FABRIC_CA_CLIENT_TLS_CERTFILES
 echo $FABRIC_CA_CLIENT_HOME/$FABRIC_CA_CLIENT_TLS_CERTFILES
-kubectl expose deployment ca-tls --type=NodePort --name=ca-tls --port 7052
-CA_SERVER_HOST=$(minikube service ca-tls --url | cut -c 8-)
-echo $CA_SERVER_HOST
+echo ./$CA_CLIENT enroll -d -u https://tls-ca-admin:tls-ca-adminpw@$CA_SERVER_HOST
 ./$CA_CLIENT enroll -d -u https://tls-ca-admin:tls-ca-adminpw@$CA_SERVER_HOST
 
 
