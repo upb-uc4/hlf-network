7f8cf34a222b7d16ecea7304481b964ae630368f
Jonas Thiele <JonasThi97@gmail.com>
2020-06-11 20:18:41 +0200

    Fix scripts

--- k8s-setup/deleteNetwork.sh
+++ k8s-setup/deleteNetwork.sh
@@ -3,10 +3,15 @@
 source ./env.sh
 
 # Delete CA Server
+echo Deleting tls root ca deployment
 kubectl delete -f tls-ca.yaml
+echo Waiting until all pods are terminated
+kubectl wait --for=delete pod -l app=ca-tls-root --timeout=60s
 
 # Delete CA Service
+echo Deleting tls root ca service
 kubectl delete -f tls-ca-service.yaml
 
 # Remove tmp dir
+echo Deleting tmp directory
 rm -rf $TMP_FOLDER
--- k8s-setup/startNetwork.sh
+++ k8s-setup/startNetwork.sh
@@ -1,7 +1,7 @@
 # Function definitions
 
 get_pods() {
-    kubectl get pods -l app=ca-tls-root --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}'
+    kubectl get pods -l app=ca-tls-root --field-selector status.phase=Running  --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' | head -n 1
 }
 
 sep() {
@@ -9,40 +9,58 @@ sep() {
 }
 
 
-# Setting environment variables
+# Set environment variables
 source ./env.sh
 
-# Starting the Network
-echo Starting Network
-minikube start
+# Start minikube
+if minikube status | grep -q 'host: Stopped'; then
+  echo Starting Network
+  minikube start
+  sep
+fi
 
-sep
 
-echo Creating TLS CA pod
-kubectl create -f tls-ca.yaml
-TLS_CA_NAME=$(get_pods)
-echo Pod $TLS_CA_NAME created
 
-sep
+# Create deployment for tls root ca
+if (($(kubectl get deployment -l app=ca-tls-root --ignore-not-found | wc -l) < 2)); then
+  echo Creating TLS CA deployment
+  kubectl create -f tls-ca.yaml
+else
+  echo TLS CA deployment already exists
+fi
 
-echo Creating TLS CA service
-kubectl create -f tls-ca-service.yaml
+
+
+# Expose service for tls root ca
+if (($(kubectl get service -l app=ca-tls-root --ignore-not-found | wc -l) < 2)); then
+  echo Creating TLS CA service
+  kubectl create -f tls-ca-service.yaml
+else
+  echo TLS CA service already exists
+fi
 CA_SERVER_HOST=$(minikube service ca-tls --url | cut -c 8-)
 echo TLS CA service exposed on $CA_SERVER_HOST
-
 sep
 
+
+# Wait until pod is ready
 echo Waiting for pod
 kubectl wait --for=condition=ready pod -l app=ca-tls-root --timeout=60s
-
+TLS_CA_NAME=$(get_pods)
+echo Using pod $TLS_CA_NAME
 sep
 
+
+
+# Copy TLS certificate into local tmp folder
 echo Copy TLS certificate to local folder
 mkdir -p $TMP_FOLDER
+mkdir -p $FABRIC_CA_CLIENT_HOME
 kubectl cp default/$TLS_CA_NAME:etc/hyperledger/fabric-ca-server/ca-cert.pem $TMP_FOLDER/ca-cert.pem
-
 sep
 
+
+# Query TLS CA server to enroll an admin identity
 echo Use CA-client to enroll admin
 cp $TMP_FOLDER/ca-cert.pem $FABRIC_CA_CLIENT_HOME/$FABRIC_CA_CLIENT_TLS_CERTFILES
 echo $FABRIC_CA_CLIENT_HOME/$FABRIC_CA_CLIENT_TLS_CERTFILES
