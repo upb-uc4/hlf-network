0010aad843960f1505cb874fc50e3ca4b8a645f1
Paul Kramer <pkramer1503@gmail.com>
2020-07-12 22:03:52 +0200

    Add msp creation for orderer setup.

--- startNetwork.sh
+++ startNetwork.sh
@@ -514,10 +514,41 @@ setup-orderer() {
   sep "Generate genesis block"
   ./configtxgen -configPath $K8S/ -profile OrgsOrdererGenesis -outputBlock $TMP_FOLDER/hyperledger/org0/orderer/genesis.block -channelID syschannel
   ./configtxgen -configPath $K8S/  -profile OrgsChannel -outputCreateChannelTx $TMP_FOLDER/hyperledger/org0/orderer/channel.tx -channelID mychannel
+}
 
-  # Move certificates to dedicated msp folders
+setup-orderer-msp() {
+  # Create MSP directory for org0
+  export MSP_DIR=$TMP_FOLDER/hyperledger/org0/msp
+  mkdir -p $MSP_DIR
+  mkdir -p $MSP_DIR/admincerts
+  mkdir -p $MSP_DIR/cacerts
+  mkdir -p $MSP_DIR/tlscacerts
+  mkdir -p $MSP_DIR/users
+  cp $TMP_FOLDER/hyperledger/org0/admin/msp/signcerts/cert.pem $MSP_DIR/admincerts/admin-org0-cert.pem
+  cp $TMP_FOLDER/hyperledger/org0/ca/crypto/ca-cert.pem $MSP_DIR/cacerts/org0-ca-cert.pem
+  cp $TMP_FOLDER/ca-cert.pem $MSP_DIR/tlscacerts/tls-ca-cert.pem
 
+  # Create MSP directory for org1
+  export MSP_DIR=$TMP_FOLDER/hyperledger/org1/msp
+  mkdir -p $MSP_DIR
+  mkdir -p $MSP_DIR/admincerts
+  mkdir -p $MSP_DIR/cacerts
+  mkdir -p $MSP_DIR/tlscacerts
+  mkdir -p $MSP_DIR/users
+  cp $TMP_FOLDER/hyperledger/org1/admin/msp/signcerts/cert.pem $MSP_DIR/admincerts/admin-org1-cert.pem
+  cp $TMP_FOLDER/hyperledger/org1/ca/crypto/ca-cert.pem $MSP_DIR/cacerts/org1-ca-cert.pem
+  cp $TMP_FOLDER/ca-cert.pem $MSP_DIR/tlscacerts/tls-ca-cert.pem
 
+  # Create MSP directory for org2
+  export MSP_DIR=$TMP_FOLDER/hyperledger/org2/msp
+  mkdir -p $MSP_DIR
+  mkdir -p $MSP_DIR/admincerts
+  mkdir -p $MSP_DIR/cacerts
+  mkdir -p $MSP_DIR/tlscacerts
+  mkdir -p $MSP_DIR/users
+  cp $TMP_FOLDER/hyperledger/org2/admin/msp/signcerts/cert.pem $MSP_DIR/admincerts/admin-org2-cert.pem
+  cp $TMP_FOLDER/hyperledger/org2/ca/crypto/ca-cert.pem $MSP_DIR/cacerts/org2-ca-cert.pem
+  cp $TMP_FOLDER/ca-cert.pem $MSP_DIR/tlscacerts/tls-ca-cert.pem
 }
 
 # Debug commands using -d flag
@@ -561,6 +592,7 @@ start-org1-peer2
 start-org2-peer1
 start-org2-peer2
 setup-orderer
+setup-orderer-msp
 
 sep
 
