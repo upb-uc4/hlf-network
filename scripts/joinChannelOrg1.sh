0e6017d475c40f430965f52e6d8bb463f8ca8888
anemonekk <anemone@kampkoetter.de>
2020-07-28 15:37:32 +0200

    Add listen peer address, removed core peer address from joinchannel script.
    Co-authored-by: Paul Kramer <pkramer1@mail.uni-paderborn.de>

--- scripts/joinChannelOrg1.sh
+++ scripts/joinChannelOrg1.sh
@@ -1,5 +1,5 @@
 export CORE_PEER_MSPCONFIGPATH=/tmp/hyperledger/org1/admin/msp
-export CORE_PEER_ADDRESS=peer1-org1:7051
+#export CORE_PEER_ADDRESS=peer1-org1:7051
 peer channel join -b /tmp/hyperledger/org1/peer1/assets/mychannel.block
 
 export CORE_PEER_ADDRESS=peer2-org1:7051
