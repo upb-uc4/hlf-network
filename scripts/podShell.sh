bc6ebc2dba11fa9a4f7cabdd3bfff221906a790f
Jonas Thiele <JonasThi97@gmail.com>
2020-07-29 11:02:00 +0200

    Replace cli1SH scripts with generic script

--- scripts/podShell.sh
+++ scripts/podShell.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+
 get_pods() {
   #1 - app name
   kubectl get pods -l app=$1 --field-selector status.phase=Running -n hlf-production-network --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' | head -n 1
 }
 
-CLI1=$(get_pods "cli-org1")
-
-# Use CLI shell
-kubectl exec -n hlf-production-network $CLI1 -it -- sh
+if [ -z "$1" ]
+then
+  echo "Usage: ./podShell.sh deployment-name"
+else
+  kubectl exec -n hlf-production-network $(get_pods "$1")  -it -- sh
+fi
