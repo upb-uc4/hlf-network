0aa5c99253f4fb74a880da4d799058df78f8d241
Paul Kramer <pkramer1503@gmail.com>
2020-09-01 09:03:34 +0200

    Fix typo.

--- scripts/startPeers.sh
+++ scripts/startPeers.sh
@@ -18,7 +18,7 @@ kubectl wait --for=condition=ready pod -l app=peer2-org1 --timeout=120s -n hlf-p
 
 sep
 
-echo "org2-peer!"
+echo "org2-peer1"
 kubectl create -f "$K8S/org2-peer1/org2-peer1.yaml" -n hlf-production-network
 kubectl create -f "$K8S/org2-peer1/org2-peer1-service.yaml" -n hlf-production-network
 kubectl wait --for=condition=ready pod -l app=peer1-org2 --timeout=120s -n hlf-production-network
