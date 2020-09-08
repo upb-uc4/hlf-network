e4b5af4002ec18d8b8bdf215ba82bd092f9ebf58
Paul Kramer <pkramer1503@gmail.com>
2020-09-08 10:08:48 +0200

    Set path to server path in env.sh

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 # This is the path to the mount folder
-export K8S_MOUNT=/data/uc4/development
+export K8S_MOUNT=/data/experimental
 export HL_MOUNT=$K8S_MOUNT/hyperledger
 export CA_CLIENT=fabric-ca-client
 export K8S=k8s
