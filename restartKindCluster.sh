810a5a0a0f1bacc6fed5f9f288ad16815ded90b5
Paul Kramer <pkramer1503@gmail.com>
2020-10-01 13:40:20 +0200

    Change the KinD utility cluster to be independent of the env.sh script.

--- restartKindCluster.sh
+++ restartKindCluster.sh
@@ -1,15 +1,12 @@
-source env.sh
+# This script is for development only and allows easily starting and restarting a kubernetes cluster on KinD
+# For production, deploy the cluster first and use the deploy script to start the network
 
 set +e
 kind delete clusters kind
-sudo rm -rf $K8S_MOUNT
+sudo rm -rf /data/development
 
 set -e
-sudo mkdir -p $HL_MOUNT
-sudo chmod -R 777 $K8S_MOUNT
+sudo mkdir -p /data/development/hyperledger
+sudo chmod -R 777 /data/development
 
-./scripts/setMountFolder.sh
 kind create cluster --config kind.yaml
-
-./startNetwork.sh
-./installChaincode.sh
