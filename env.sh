81b16a9b7f9d8c96237e6f7969ff8adb58555c8d
Paul Kramer <pkramer1503@gmail.com>
2020-10-01 13:43:24 +0200

    Change default paths in env.sh.

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 # This is the path to the mount folder
-export K8S_MOUNT=/data/experimental
-export HL_MOUNT=$K8S_MOUNT/hyperledger
+export K8S_MOUNT=/data/development
+export HL_MOUNT=/data/development/hyperledger
 export CA_CLIENT=fabric-ca-client
 export K8S=k8s
 export CONTAINER_TIMEOUT=300s
