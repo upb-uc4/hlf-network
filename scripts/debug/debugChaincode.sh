7d5913909d43e12d215ff426bb9b3e764b2d06c5
Paul Kramer <pkramer1503@gmail.com>
2020-10-02 20:11:27 +0200

    Fix debugChaincode.sh paths.

--- scripts/debug/debugChaincode.sh
+++ scripts/debug/debugChaincode.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 get_pods() {
   #1 - app name
   kubectl get pods -l app=$1 --field-selector status.phase=Running -n hlf-production-network --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' | head -n 1
@@ -8,6 +10,6 @@ set -e
 
 source ./env.sh
 
-kubectl exec -n hlf-production-network $(get_pods "cli-org1") -i -- sh < scripts/debug/queryInstalledOrg1.sh
-kubectl exec -n hlf-production-network $(get_pods "cli-org2") -i -- sh < scripts/debug/queryInstalledOrg2.sh
-kubectl exec -n hlf-production-network $(get_pods "cli-org1") -i -- sh < scripts/debug/queryCommited.sh
+kubectl exec -n hlf-production-network $(get_pods "cli-org1") -i -- sh < scripts/debug/debugChaincode/queryInstalledOrg1.sh
+kubectl exec -n hlf-production-network $(get_pods "cli-org2") -i -- sh < scripts/debug/debugChaincode/queryInstalledOrg2.sh
+kubectl exec -n hlf-production-network $(get_pods "cli-org1") -i -- sh < scripts/debug/debugChaincode/queryCommited.sh
