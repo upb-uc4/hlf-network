e86dd27c6cd69270d24b019122845e85b28ad40c
Paul Kramer <pkramer1503@gmail.com>
2020-10-02 16:43:53 +0200

    Merged changes.

--- scripts/setMountFolder.sh
+++ scripts/setMountFolder.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-source env.sh
+source ./env.sh
 
 rm configtx.yaml
-rm kind.yaml
 envsubst \$HL_MOUNT < configtx-template.yaml > configtx.yaml
-envsubst \$HL_MOUNT < kind-template.yaml > kind.yaml
