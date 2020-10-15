8cee55db11b1b30ce372b5ed4ad8eddf645a344e
Paul Kramer <pkramer1503@gmail.com>
2020-10-15 21:37:57 +0200

    Refactor setupPeers.sh.

--- scripts/startNetwork/setupPeers.sh
+++ scripts/startNetwork/setupPeers.sh
@@ -4,35 +4,26 @@ source ./scripts/util.sh
 source ./scripts/env.sh
 
 
-header "Enroll peers"
-
-# TODO use secrete for distribution of root certificate
-
-# Run kubernetes jobs to enroll peers
-kubectl create -f k8s/org1/enroll-peers-org1.yaml
-kubectl create -f k8s/org2/enroll-peers-org2.yaml
-kubectl wait --for=condition=complete job -l app=enroll-peers --timeout=${CONTAINER_TIMEOUT} -n hlf
-
 header "Starting Peers"
 
 echo "org1-peer1"
 kubectl create -f k8s/org1/peer1-org1.yaml
-kubectl wait --for=condition=ready pod -l app=peer1-org1 --timeout=${CONTAINER_TIMEOUT} -n hlf
-
-sep
+small_sep
 
 echo "org1-peer2"
 kubectl create -f k8s/org1/peer2-org1.yaml
-kubectl wait --for=condition=ready pod -l app=peer2-org1 --timeout=${CONTAINER_TIMEOUT} -n hlf
-
-sep
+small_sep
 
 echo "org2-peer1"
 kubectl create -f k8s/org2/peer1-org2.yaml
-kubectl wait --for=condition=ready pod -l app=peer1-org2 --timeout=${CONTAINER_TIMEOUT} -n hlf
-
-sep
+small_sep
 
 echo "org2-peer2"
 kubectl create -f k8s/org2/peer2-org2.yaml
-kubectl wait --for=condition=ready pod -l app=peer2-org2 --timeout=${CONTAINER_TIMEOUT} -n hlf
+small_sep
+
+echo "Wait until pods of all peers are ready"
+kubectl wait --for=condition=ready pod -l app=peer1-org1 --timeout=${CONTAINER_TIMEOUT} -n hlf
+kubectl wait --for=condition=ready pod -l app=peer2-org1 --timeout=${CONTAINER_TIMEOUT} -n hlf
+kubectl wait --for=condition=ready pod -l app=peer1-org2 --timeout=${CONTAINER_TIMEOUT} -n hlf
+kubectl wait --for=condition=ready pod -l app=peer1-org2 --timeout=${CONTAINER_TIMEOUT} -n hlf
