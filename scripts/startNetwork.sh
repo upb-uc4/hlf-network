c1c228bb2c8101cc404526a4022f682816dbb74b
Paul Kramer <pkramer1503@gmail.com>
2020-10-15 21:40:30 +0200

    Comment copying of certificates for api in startNetwork.sh.

--- scripts/startNetwork.sh
+++ scripts/startNetwork.sh
@@ -29,35 +29,37 @@ source ./scripts/startNetwork/setupTlsCa.sh
 source ./scripts/startNetwork/setupOrdererOrgCa.sh
 source ./scripts/startNetwork/setupOrg1Ca.sh
 source ./scripts/startNetwork/setupOrg2Ca.sh
+source ./scripts/startNetwork/startClis.sh
 source ./scripts/startNetwork/setupPeers.sh
 source ./scripts/startNetwork/setupOrderer.sh
-source ./scripts/startNetwork/startClis.sh
 source ./scripts/startNetwork/setupDind.sh
 source ./scripts/startNetwork/setupChannel.sh
 
 # For scala api on kubernetes
 mkdir -p $HL_MOUNT/api
 cp assets/connection_profile_kubernetes.yaml $HL_MOUNT/api
+
+# TODO figure something out for distributing certificates :)
 #cp $HL_MOUNT/ca-cert.pem $HL_MOUNT/api/ca-cert.pem
-mkdir -p $HL_MOUNT/api/org0/msp
-mkdir -p $HL_MOUNT/api/org1/msp
-mkdir -p $HL_MOUNT/api/org2/msp
-cp -r $HL_MOUNT/org0/msp $HL_MOUNT/api/org0/
-cp -r $HL_MOUNT/org1/msp $HL_MOUNT/api/org1/
-cp -r $HL_MOUNT/org2/msp $HL_MOUNT/api/org2/
+# mkdir -p $HL_MOUNT/api/org0/msp
+# mkdir -p $HL_MOUNT/api/org1/msp
+# mkdir -p $HL_MOUNT/api/org2/msp
+# cp -r $HL_MOUNT/org0/msp $HL_MOUNT/api/org0/
+# cp -r $HL_MOUNT/org1/msp $HL_MOUNT/api/org1/
+# cp -r $HL_MOUNT/org2/msp $HL_MOUNT/api/org2/
 
 # For scala api locally
-set +e
-rm -rf /tmp/hyperledger/
-mkdir -p /tmp/hyperledger/
-mkdir -p /tmp/hyperledger/org0
-mkdir -p /tmp/hyperledger/org1
-mkdir -p /tmp/hyperledger/org2
-#cp $HL_MOUNT/ca-cert.pem /tmp/hyperledger/
-cp -a $HL_MOUNT/org0/msp /tmp/hyperledger/org0
-cp -a $HL_MOUNT/org1/msp /tmp/hyperledger/org1
-cp -a $HL_MOUNT/org2/msp /tmp/hyperledger/org2
-set -e
+# set +e
+# jrm -rf /tmp/hyperledger/
+# mkdir -p /tmp/hyperledger/
+# mkdir -p /tmp/hyperledger/org0
+# mkdir -p /tmp/hyperledger/org1
+# mkdir -p /tmp/hyperledger/org2
+# cp $HL_MOUNT/ca-cert.pem /tmp/hyperledger/
+# cp -a $HL_MOUNT/org0/msp /tmp/hyperledger/org0
+# cp -a $HL_MOUNT/org1/msp /tmp/hyperledger/org1
+# cp -a $HL_MOUNT/org2/msp /tmp/hyperledger/org2
+# set -e
 
 sep
 echo "Done!"
