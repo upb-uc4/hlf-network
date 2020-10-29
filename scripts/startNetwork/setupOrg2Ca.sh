dc54297d21e32599ff3a1980796426afcf0d3076
Jonas Thiele <JonasThi97@gmail.com>
2020-10-29 13:33:34 +0100

    Run registerOrg2CaUsers from PostStart hook

--- scripts/startNetwork/setupOrg2Ca.sh
+++ scripts/startNetwork/setupOrg2Ca.sh
@@ -5,17 +5,8 @@ source ./scripts/env.sh
 
 header "Org2 CA"
 
-# Create deployment for org2 ca
-echo "Creating Org2 CA"
+msg "Starting Org2 CA"
 kubectl create -f k8s/org2/rca-org2.yaml
 
-small_sep
-
-# Wait until pod is ready
-echo "Waiting for pod"
+msg "Waiting for pod"
 kubectl wait --for=condition=ready pod -l app=rca-org2 --timeout=${CONTAINER_TIMEOUT} -n hlf
-sleep $SERVER_STARTUP_TIME
-
-small_sep
-
-kubectl exec -n hlf $(get_pods "rca-org2") -i -- bash /tmp/hyperledger/scripts/startNetwork/registerUsers/registerOrg2CaUsers.sh
