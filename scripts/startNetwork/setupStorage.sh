9ed7801244faa5a38c56d158cbafbe551a75ed91
anemonekk <anemone@kampkoetter.de>
2020-12-04 18:09:20 +0100

    Add persistent storage for peer2-org1.
    Co-authored-by: Paul Kramer <pkramer1@mail.uni-paderborn.de>

--- scripts/startNetwork/setupStorage.sh
+++ scripts/startNetwork/setupStorage.sh
@@ -6,3 +6,4 @@ kubectl create -f k8s/org0/orderer-org0-storage.yaml
 kubectl create -f k8s/org1/rca-org1-storage.yaml
 kubectl create -f k8s/org1/peer1-org1-storage.yaml
 kubectl create -f k8s/org2/rca-org2-storage.yaml
+kubectl create -f k8s/org1/peer2-org1-storage.yaml
